67ce1c52f81072d918fb274f17b473f341e7c0c9
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-09 15:55:11 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import { Box, useTheme, useMediaQuery } from "@mui/material";
+import { Container, Box } from "@mui/material";
 import ScanCard from "./components/ScanCard";
 import { useScan } from "./hooks/useScan";
 
 function App() {
   const scanState = useScan();
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
     <Box
       sx={{
-        width: "100%",
-        maxWidth: isMobile ? "100vw" : 500,
-        px: isMobile ? 2 : 3,
-        mt: 4,
-        mb: 4,
+        minHeight: "100vh",
         display: "flex",
+        alignItems: "center",
         justifyContent: "center",
+        px: 2,
       }}
     >
-      <ScanCard {...scanState} />
+      <Container maxWidth="md">
+        <ScanCard {...scanState} />
+      </Container>
     </Box>
   );
 }
