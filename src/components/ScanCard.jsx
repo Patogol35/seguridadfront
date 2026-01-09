4d6c60e51f6c6cb7bf2fd3376f57552380ad47bc
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-09 14:20:02 -0500

    Update ScanCard.jsx

--- src/components/ScanCard.jsx
+++ src/components/ScanCard.jsx
@@ -14,6 +14,7 @@ import SecurityIcon from "@mui/icons-material/Security";
 import WarningIcon from "@mui/icons-material/Warning";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { motion } from "framer-motion";
+
 export default function ScanCard({
   url,
   setUrl,
@@ -27,11 +28,9 @@ export default function ScanCard({
     if (level === "MEDIO") return "warning";
     return "success";
   };
+
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 30 }}
-      animate={{ opacity: 1, y: 0 }}
-    >
+    <motion.div initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }}>
       <Card sx={{ borderRadius: 4, boxShadow: 6 }}>
         <CardContent>
           <Box display="flex" alignItems="center" mb={2}>
@@ -40,30 +39,34 @@ export default function ScanCard({
               Web Security Analyzer
             </Typography>
           </Box>
+
           <TextField
             fullWidth
             label="URL del sitio"
             value={url}
             onChange={(e) => setUrl(e.target.value)}
           />
+
           <Button
             fullWidth
             variant="contained"
-            size="large"
             sx={{ mt: 3 }}
             onClick={scan}
             disabled={!url || loading}
           >
             {loading ? <CircularProgress size={24} /> : "Escanear"}
           </Button>
+
           {error && (
             <Typography color="error" mt={2}>
               {error}
             </Typography>
           )}
+
           {result && (
             <>
               <Divider sx={{ my: 3 }} />
+
               <Chip
                 label={`Riesgo: ${result.risk.level}`}
                 color={riskColor(result.risk.level)}
@@ -75,6 +78,7 @@ export default function ScanCard({
                   )
                 }
               />
+
               <Stack spacing={1} mt={2}>
                 <Typography>
                   XSS:{" "}
@@ -88,6 +92,12 @@ export default function ScanCard({
                     {result.results.sqli.vulnerable ? "Vulnerable" : "Seguro"}
                   </strong>
                 </Typography>
+                <Typography>
+                  Headers faltantes:{" "}
+                  <strong>
+                    {result.results.headers.missing_headers.length}
+                  </strong>
+                </Typography>
               </Stack>
             </>
           )}
@@ -95,4 +105,4 @@ export default function ScanCard({
       </Card>
     </motion.div>
   );
-}
+                            }
