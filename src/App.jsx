92bce1ef084cc2a6dee8a4d934e21dcd5eb03f0c
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-09 16:21:15 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Container, Box } from "@mui/material";
+import { Container } from "@mui/material";
 import ScanCard from "./components/ScanCard";
 import { useScan } from "./hooks/useScan";
 
@@ -6,19 +6,9 @@ function App() {
   const scanState = useScan();
 
   return (
-    <Box
-      sx={{
-        minHeight: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        px: 2,
-      }}
-    >
-      <Container maxWidth="md">
-        <ScanCard {...scanState} />
-      </Container>
-    </Box>
+    <Container maxWidth="sm" sx={{ mt: 6 }}>
+      <ScanCard {...scanState} />
+    </Container>
   );
 }
 
