a810f48881c301088cc69ecb7de66d83e1e73756
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-09 15:33:16 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
-import { Container, Box, useMediaQuery } from "@mui/material";
+import { Container } from "@mui/material";
 import ScanCard from "./components/ScanCard";
 import { useScan } from "./hooks/useScan";
 
 function App() {
   const scanState = useScan();
-  const isLandscape = useMediaQuery("(orientation: landscape)");
 
   return (
     <Container
-      maxWidth={false}
-      disableGutters
+      maxWidth="md"
       sx={{
         minHeight: "100vh",
-        px: { xs: 1, sm: 2 },
+        py: { xs: 2, sm: 4 },
+        display: "flex",
+        justifyContent: "center",
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: isLandscape ? "flex-start" : "center",
-          minHeight: "100vh",
-          pt: isLandscape ? 1 : 0,
-        }}
-      >
-        <ScanCard {...scanState} isLandscape={isLandscape} />
-      </Box>
+      <ScanCard {...scanState} />
     </Container>
   );
 }
