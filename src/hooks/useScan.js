d2f941abedf314ec20f0b4255d70a4b67db6688f
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-09 14:19:31 -0500

    Update useScan.js

--- src/hooks/useScan.js
+++ src/hooks/useScan.js
@@ -1,29 +1,28 @@
 import { useState } from "react";
 import { scanWebsite } from "../api/scanApi";
+
 export const useScan = () => {
   const [url, setUrl] = useState("");
   const [loading, setLoading] = useState(false);
   const [result, setResult] = useState(null);
   const [error, setError] = useState(null);
+
   const scan = async () => {
+    setLoading(true);
     setError(null);
     setResult(null);
-    setLoading(true);
+
     try {
       const data = await scanWebsite(url);
       setResult(data);
-    } catch {
-      setError("Error al escanear el sitio");
+    } catch (err) {
+      setError(
+        err.response?.data?.detail || "Error al escanear el sitio"
+      );
     } finally {
       setLoading(false);
     }
   };
-  return {
-    url,
-    setUrl,
-    loading,
-    result,
-    error,
-    scan,
-  };
+
+  return { url, setUrl, loading, result, error, scan };
 };
