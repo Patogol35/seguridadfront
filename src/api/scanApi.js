2e969330b70e019a08ccb618eacadae1d5e5a04e
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-09 14:12:24 -0500

    Update scanApi.js

--- src/api/scanApi.js
+++ src/api/scanApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "https://seguridad-5s8y.onrender.com/docs";
+const API_URL = "https://seguridad-5s8y.onrender.com";
 export const scanWebsite = async (url) => {
   const response = await axios.post(`${API_URL}/scan`, { url });
   return response.data;
